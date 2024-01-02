f6fefac273f00aea54db26a946c71924b834f8d3
Luukbrouwer <luuk904@gmail.com>
2024-01-02 17:33:25 +0100

    input connection with arduino
    in het script 'input' staat nu de code voor de connection met arduino voor de lengte in het bloedvat

---
+++ bloedvat/Assets/assets bloedvat/Input.cs
@@ -0,0 +1,79 @@
+//=========================================
+//dit is het script voor de input
+//op dit moment is dat spatiebalk die een kracht toevoegd aan de guidewire
+//=========================================
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+
+
+
+
+public class Movement : MonoBehaviour
+{
+    //start arduino connection:
+    SerialPort data_stream =new SerialPort("COM4" , 115200);
+    public string receivedstring;
+    public Rigidbody rb;
+    public string[] datas;
+    
+    private Vector3 beginGuwi;
+    
+    //======================
+    //spatiebalk
+    public float ForceApplied = -20f;
+    //==========================
+
+    
+    void Start()
+    {
+        //for arduino connection:
+        data_stream.Open(); //initiate serial monitor "stream"
+        beginGuwi= rb.transform.position;
+    }
+
+
+    void Update()
+    {
+        //for arduino connection:
+        receivedstring = data_stream.ReadLine();
+
+        string[] datas = receivedstring.Split(";"); //split data tussen ';'
+        float length= float.Parse(datas[0]) *0.00001f;
+        transform.position= new Vector3 (beginGuwi[0], beginGuwi[1]-length, beginGuwi[2]); 
+        
+    }
+
+
+    void FixedUpdate() //spatiebalk...
+    {
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, -ForceApplied * Time.deltaTime, 0);
+        }
+
+       
+
+    }
+
+    void OnApplicationQuit()
+    {
+       data_stream.Close(); 
+    }
+
+}
+
+
+    
+
+    
+
+
+
+
